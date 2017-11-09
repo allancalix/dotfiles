3d429fef824137cd792f7258dc89e653d96840c5
Allan Calix <acalix0592@gmail.com>
2017-11-09 01:26:05 -0800

    Add a nifty function to fuzzy find branches in a git repo

--- zsh/git.sh
+++ zsh/git.sh
@@ -86,3 +86,12 @@ gupstation() {
   gf production
   gf staging
 }
+
+# fbr - checkout git branch (including remote branches)
+gbrs() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
