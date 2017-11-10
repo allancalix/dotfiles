d38332e00da01fb015c53c4711ae0570aa2c2f17
Allan Calix <acalix0592@gmail.com>
2017-11-09 23:27:52 -0800

    Slight tweaks for a making better use of space in workflow

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -1,5 +1,7 @@
 export ZSH=$HOME/.oh-my-zsh
 export GIT_EDITOR="vim"
+export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+
 CUST_ZSH=$HOME/.zsh
 ZSH_THEME="spaceship"
 
@@ -10,9 +12,9 @@ source "$CUST_ZSH/completion.sh"
 source "$CUST_ZSH/aliases.sh"
 source "$CUST_ZSH/git.sh"
 
-# Env vars
+# Allow for different env variables depending on system
 if [ $(uname -s) = 'Darwin' ]; then
-  export PATH="$HOME/bin:/usr/local/bin:$PATH"
+  export PATH="$HOME/bin:/$HOME/bin/cargo:$HOME/bin/go:usr/local/bin:$PATH"
 else
   export PATH="$HOME/bin:/$HOME/bin/cargo:$HOME/bin/go:usr/local/bin:$PATH"
 fi
@@ -22,6 +24,6 @@ bindkey -s '^f' 'fe\n'
 bindkey -s '^e' 'cdf\n'
 
 export NVM_DIR="$HOME/.nvm"
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
