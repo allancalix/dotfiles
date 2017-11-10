5f8afcb1925f92a588a1ba68a6ea2fcf9b0633f2
Allan Calix <acalix0592@gmail.com>
2017-11-09 23:42:13 -0800

    Use ripgrep instead of grep for filtering branch names

--- zsh/git.sh
+++ zsh/git.sh
@@ -90,7 +90,7 @@ gupstation() {
 # fbr - checkout git branch (including remote branches)
 gbrs() {
   local branches branch
-  branches=$(git branch --all | grep -v HEAD) &&
+  branches=$(git branch --all | rg -v HEAD) &&
   branch=$(echo "$branches" |
            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
