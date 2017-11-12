3763e97c6f354f4f60a32d5fe1ac2716c4e879e1
Allan Calix <acalix0592@gmail.com>
2017-11-11 18:59:42 -0800

    Make the fuzzy file finder more obtuse so I can see it easier

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -1,6 +1,5 @@
 export ZSH=$HOME/.oh-my-zsh
 export GIT_EDITOR="vim"
-export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 
 CUST_ZSH=$HOME/.zsh
 ZSH_THEME="spaceship"
@@ -24,6 +23,7 @@ bindkey -s '^f' 'fe\n'
 bindkey -s '^e' 'cdf\n'
 
 export NVM_DIR="$HOME/.nvm"
+
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
