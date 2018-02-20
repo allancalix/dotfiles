8c312473de755f393ec76420be0cb8158c84407a
Allan Calix <acalix0592@gmail.com>
2018-02-19 22:21:57 -0800

    Add pyenv configs to zsh config

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -2,6 +2,7 @@ export ZSH=$HOME/.oh-my-zsh
 export VISUAL="nvim"
 export EDITOR="$VISUAL"
 export GIT_EDITOR="nvim"
+export PYENV_ROOT="$HOME/.pyenv"
 
 CUST_ZSH=$HOME/.zsh
 ZSH_THEME="spaceship"
@@ -15,9 +16,9 @@ source "$CUST_ZSH/git.sh"
 
 # Allow for different env variables depending on system
 if [ $(uname -s) = 'Darwin' ]; then
-  export PATH="$HOME/bin:/$HOME/bin/cargo:$HOME/bin/go:usr/local/bin:$PATH"
+  export PATH="$HOME/bin:$HOME/bin/cargo:$HOME/go/bin:usr/local/bin:$PYENV_ROOT/bin:$PATH"
 else
-  export PATH="$HOME/bin:/$HOME/bin/cargo:$HOME/bin/go:usr/local/bin:$PATH"
+  export PATH="$HOME/bin:$HOME/bin/cargo:$HOME/go/bin:usr/local/bin:$PYENV_ROOT/bin:$PATH"
 fi
 
 # Keybindings
@@ -26,6 +27,10 @@ bindkey -s '^e' 'cdf\n'
 
 export NVM_DIR="$HOME/.nvm"
 source $NVM_DIR/nvm.sh
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
 
 export FZF_DEFAULT_COMMAND="rg --files --hidden -g '!.git/'"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
