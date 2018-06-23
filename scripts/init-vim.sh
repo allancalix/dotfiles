001d2f6516deee3b112909b29b87ee000f851661
Allan Calix <contact@allancalix.com>
2018-06-23 16:10:45 -0700

    Use .vim folder in vim scripts

--- scripts/init-vim.sh
+++ scripts/init-vim.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 readonly pkg_manager="https://github.com/k-takata/minpac.git"
 
 install_package_manager() {
-  pkg_path="vim/pack/minpac/opt"
-  mkdir -p "vim/tmp"
+  pkg_path=".vim/pack/minpac/opt"
+  mkdir -p ".vim/tmp"
   mkdir -p "$pkg_path"
   git clone "$pkg_manager" "$pkg_path""/minpac"
 }
 
 init_vim() {
-  rm -rf vim/pack
+  rm -rf .vim/pack
   install_package_manager
 }
 
