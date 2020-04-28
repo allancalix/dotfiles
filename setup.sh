9ca768af97dfb10ed4238f70b1d129b9d2d4f9b9
Allan Calix <allancalix@Allans-MacBook-Pro.local>
2020-04-28 13:54:10 -0700

    Move dotfiles sync to ansible

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ done < "REQUIREMENTS"
 readonly POST_INSTALL="POST-INSTALL.sh"
 
 sync_configs() {
-  rsync -avh --no-perms --exclude=".zsh.local" dotfiles/ "$HOME";
-  rsync -avh --no-perms --ignore-existing dotfiles/.zsh.local "$HOME";
-
   # This _will_ override an existing config
   ln -sf "$PWD"/extra/vscode-settings.json "$VS_CODE_DEST"
 }
@@ -19,8 +16,6 @@ main() {
   vim::install_package_manager
   zsh::install_prompt
 
-  echo "Syncing configs..."
-  sync_configs
   echo "Installing additional dependencies..."
   vim::install_packages
   [[ -r "$POST_INSTALL" ]] && [[ -f "$POST_INSTALL" ]] && source "$POST_INSTALL"
