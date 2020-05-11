aa5379dca8089a578e6e0e45ae9b1f1a848ca3ca
Allan Calix <contact@allancalix.com>
2020-05-10 23:39:38 -0700

    Port more aliases from zsh

---
+++ dotfiles/.config/fish/aliases.fish
@@ -0,0 +1,29 @@
+# General
+abbr -a c cargo
+abbr -a vi nvim
+abbr -a m make
+
+# Git
+abbr -a d 'gd $argv'
+abbr -a s 'git status -sb'
+
+abbr -a gd 'git diff -M'
+abbr -a gdc 'git diff --cached -M'
+abbr -a ga 'git add -A'
+abbr -a gap 'ga -p'
+abbr -a gau 'git add -u'
+abbr -a gbr 'git branch -v'
+abbr -a gc! 'git commit -v'
+abbr -a gl 'git lg'
+abbr -a gst 'git stash'
+abbr -a gstp 'git stash pop'
+abbr -a gup 'git pull'
+abbr -a gf 'git fetch --prune'
+abbr -a gc 'git commit -v'
+abbr -a gstp 'git stash pop'
+abbr -a gup 'git pull'
+abbr -a gf 'git fetch --prune'
+abbr -a gc 'git commit -v'
+
+# Kubernetes
+abbr -a k 'kubectl'
--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -5,12 +5,12 @@ setenv TERMCOLOR "truecolor"
 setenv PYENV_ROOT "$HOME/.pyenv"
 set PATH $PATH $HOME/bin $HOME/.cargo/bin $PYENV_ROOT/bin $HOME/go/bin
 
+set FISH_CONFIG_ROOT "$HOME/.config/fish"
+
+source "$FISH_CONFIG_ROOT/aliases.fish"
+
 # Switch to vi style line editing.
 fish_vi_key_bindings
-
-abbr -a c cargo
-abbr -a vi nvim
-abbr -a m make
 
 if command -v tmux >/dev/null
   abbr -a t 'tmux'
@@ -36,24 +36,6 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate ''
 set __fish_git_prompt_showupstream 'none'
 set -g fish_prompt_pwd_dir_length 3
-
-# Git Aliases
-abbr -a d 'gd $argv'
-abbr -a s 'git status -sb'
-
-abbr -a gd 'git diff -M'
-abbr -a gdc 'git diff --cached -M'
-abbr -a ga 'git add -A'
-abbr -a gap 'ga -p'
-abbr -a gau 'git add -u'
-abbr -a gbr 'git branch -v'
-abbr -a gc! 'git commit -v'
-abbr -a gl 'git lg'
-abbr -a gst 'git stash'
-abbr -a gstp 'git stash pop'
-abbr -a gup 'git pull'
-abbr -a gf 'git fetch --prune'
-abbr -a gc 'git commit -v'
 
 setenv FZF_DEFAULT_COMMAND "fd --type f"
 setenv FZF_DEFAULT_OPTS "
