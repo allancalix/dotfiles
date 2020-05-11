6e08afe6fe4075d476886c4721aec8d87c3a6599
Allan Calix <contact@allancalix.com>
2020-05-10 23:55:59 -0700

    Fix bug with git_current_branch function

--- dotfiles/.config/fish/functions/git_current_branch.fish
+++ dotfiles/.config/fish/functions/git_current_branch.fish
@@ -1,4 +1,5 @@
 function git_current_branch
-  cat "(git rev-parse --git-dir 2>/dev/null)/HEAD" | sed -e 's/^.*refs\/heads\///'
+  set path (git rev-parse --git-dir 2>/dev/null)
+  cat "$path/HEAD" | sed -e 's/^.*refs\/heads\///'
 end
 
