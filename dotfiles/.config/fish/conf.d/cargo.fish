70502c7a984a83738e06ce263f27a2da69269bc1
Allan Calix <allan@acx.dev>
2021-06-18 01:26:38 -0700

    Adds a role for rustup installs

--- dotfiles/.config/fish/conf.d/cargo.fish
+++ dotfiles/.config/fish/conf.d/cargo.fish
@@ -1,5 +1,6 @@
 lib::check_cmd_exists cargo
 
 setenv CARGO_HOME "$XDG_CONFIG_HOME/cargo"
+setenv RUSTUP_HOME "$XDG_CONFIG_HOME/rustup"
 setenv CARGO_INSTALL_ROOT "$HOME"
-
+fish_add_path "$XDG_CONFIG_HOME/cargo/bin"
