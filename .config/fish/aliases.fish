8a5a11cc8e3995384fc09f0a76af5bbfdc6d4d87
Allan Calix <allan@acx.dev>
2021-07-15 18:41:12 -0700

    Moves aliases to binary configuration files

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,12 +1,6 @@
 # General
 abbr -a e $EDITOR
 
-# Cargo
-abbr -a c 'cargo'
-abbr -a cr 'cargo run'
-abbr -a cb 'cargo build'
-abbr -a ct 'cargo test'
-
 # Kubernetes
 abbr -a k 'kubectl'
 abbr -a kg 'kubectl get'
@@ -15,9 +9,6 @@ abbr -a kg 'kubectl get'
 abbr -a tf 'terraform'
 abbr -a tfa 'terraform apply'
 abbr -a tfp 'terraform plan'
-
-# Tmux
-abbr -a tsw 'tmux switch -t'
 
 # Tmuxinator
 abbr -a t2s 'tmuxinator start'
--- .config/fish/conf.d/cargo.fish
+++ .config/fish/conf.d/cargo.fish
@@ -3,3 +3,8 @@ setenv RUSTUP_HOME "$XDG_CONFIG_HOME/rustup"
 setenv CARGO_INSTALL_ROOT "$HOME"
 
 lib::check_cmd_exists cargo
+
+abbr -a c 'cargo'
+abbr -a cr 'cargo run'
+abbr -a cb 'cargo build'
+abbr -a ct 'cargo test'
