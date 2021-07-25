edd1244a52b3afa6352334221b764235d7b833cc
Allan Calix <allan@acx.dev>
2021-07-25 16:22:43 -0700

    Refactors checklist toggle to support additional states

--- .config/nvim/lua/checklist.lua
+++ .config/nvim/lua/checklist.lua
@@ -1,18 +1,19 @@
 local M = {}
 
+local default_states = {
+  ' ', -- TODO
+  'x', -- DONE
+}
+
 M.toggle_item = function()
   local line = vim.api.nvim_get_current_line()
 
-  -- [ ] TODO state.
-  if line:match('%[%s+%]') then
-    local new = line:gsub('%[%s+%]', '[x]', 1)
-    return vim.api.nvim_set_current_line(new)
-  end
-
-  -- [x] DONE state.
-  if line:match('%[x%]') then
-    local new = line:gsub('%[x%]', '[ ]', 1)
-    return vim.api.nvim_set_current_line(new)
+  for i, state in ipairs(default_states) do
+    if line:match('%['.. state ..'%]') then
+      next = default_states[i + 1] or default_states[1]
+      local new = line:gsub('%['.. state ..'%]', '['.. next ..']', 1)
+      return vim.api.nvim_set_current_line(new)
+    end
   end
 end
 
