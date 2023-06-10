c9480e7c183f43728847eb3f3a50e9df13da57e0
Allan Calix <contact@acx.dev>
2023-06-10 13:17:05 -0700

    Bump nixunstable repository

--- scripts/link-gui.sh
+++ scripts/link-gui.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if command -v oc >/dev/null 2>&1; then
+  oc scripts/link_apps.ml
+  exit 0
+fi
+
 fd --maxdepth 1 . $HOME/.nix-profile/Applications --exec basename | while read -r file; do
   target="${HOME}/Applications/${file}"
 
