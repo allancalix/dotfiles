88a344d9438a908141699a204d21135c78f26a0a
Allan Calix <contact@acx.dev>
2024-03-21 22:45:01 -0700

    Remove brewfile

---
+++ tasks.clj
@@ -0,0 +1,10 @@
+(ns tasks
+  (:require [babashka.process :refer [shell sh]]))
+
+(defn render []
+  (shell "nickel export -f toml -o nix/starship/starship.toml ncl/starship.ncl"))
+
+(defn switch [_m]
+  (render)
+  (shell "home-manager switch --flake flake.nix#allancalix"))
+              
