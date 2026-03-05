1d0312e49b2211b4510b667c60f545eedf57d0a2
Allan Calix <contact@acx.dev>
2026-03-04 21:29:16 -0800

    Fix neovim zen mode layout

--- nix/nvim/lua/plugins/snacks.lua
+++ nix/nvim/lua/plugins/snacks.lua
@@ -5,12 +5,12 @@ return {
     dashboard = { enabled = false },
     explorer = { enabled = false },
     zen = {
-      win = { style = "zen" },
+      win = { style = "zen", width = 89 },
       on_open = function(win)
         vim.wo[win.win].wrap = true
         vim.wo[win.win].linebreak = true
-        vim.wo[win.win].textwidth = 0
-        win:resize({ width = 89 })
+        local buf = win.buf or vim.api.nvim_win_get_buf(win.win)
+        vim.bo[buf].textwidth = 0
       end,
     },
   },
